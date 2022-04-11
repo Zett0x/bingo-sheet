e8e72f5b69f285e69c39c6a28159cff44eb399d8
Zett0x <zetto1514@gmail.com>
2022-04-11 10:32:53 +0200

    https

--- src/api/getRandomNumber.js
+++ src/api/getRandomNumber.js
@@ -1,6 +1,6 @@
 export const getRandomnumber = async (min = 1, max = 90, count = 15) => {
   const res = await fetch(
-    `http://www.randomnumberapi.com/api/v1.0/randomredditnumber?min=${min}&max=${max}&count=${count}`
+    `https://www.randomnumberapi.com/api/v1.0/randomredditnumber?min=${min}&max=${max}&count=${count}`
   );
   const data = await res.json();
   return data;
